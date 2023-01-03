0fcdad14f79df9d6752a83693e5923b79bafc041
stefandaius <stefan.iordache2001@gmail.com>
2023-01-03 17:08:00 +0200

    add redirect to docs

--- themes/hugo-shard/assets/js/header.js
+++ themes/hugo-shard/assets/js/header.js
@@ -1,6 +1,13 @@
 var scrollPosition = window.scrollY;
 var header = document.getElementById('header');
 
+window.addEventListener("load", function() {
+  console.log(this.window.location.pathname);
+  if (this.window.location.pathname == "/docs/" || this.window.location.pathname == "/doc/") {
+    this.window.location.href = "https://docs.genez.io/";
+  }
+}, false);
+
 window.addEventListener('scroll', function() {
   scrollPosition = window.scrollY;
 
